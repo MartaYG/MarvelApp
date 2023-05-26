72e1e360da08d84c10048b8f8f1259c628307de3
myebragonzalez <myebra@hiberus.com>
2023-05-26 22:40:13 +0200

    Creation and display of the character's list view

---
+++ app/src/main/java/com/myebra/marvelapp/ui/features/characterslist/ui/CharactersListScreen.kt
@@ -0,0 +1,103 @@
+package com.myebra.marvelapp.ui.features.characterslist.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Book
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.myebra.marvelapp.domain.models.features.characters.Character
+import com.myebra.marvelapp.ui.features.characterslist.viewmodels.CharactersListViewModel
+
+@Composable
+fun CharactersListScreen(charactersListViewModel: CharactersListViewModel){
+
+    val charactersData by charactersListViewModel.allCharacters.collectAsState()
+
+    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+        items(charactersData) { item ->
+            CharacterItem(item = item)
+        }
+    }
+}
+
+@Composable
+fun CharacterItem(item : Character) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 6.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+        ) {
+            AsyncImage(
+                model = item.thumbnail,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(shape = RoundedCornerShape(4.dp))
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    text = item.name,
+                    fontWeight = FontWeight.Bold,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                    fontSize = 20.sp,
+                    modifier = Modifier
+                        .padding(top = 20.dp, start = 10.dp)
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+
+                ) {
+                    Icon(
+                        Icons.Filled.Book,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .padding(start = 10.dp)
+                    )
+                    Text(
+                        text = item.comicCount.toString(),
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 1,
+                    )
+                }
+            }
+        }
+    }
+}
