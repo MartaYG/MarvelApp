6916397d0ec275f0d205b3b116879105b6eaa881
myebragonzalez <myebra@hiberus.com>
2023-05-26 22:21:40 +0200

    Creation of data class for Characters

---
+++ app/src/main/java/com/myebra/marvelapp/domain/models/features/characters/Character.kt
@@ -0,0 +1,9 @@
+package com.myebra.marvelapp.domain.models.features.characters
+
+data class Character (
+    val characterId : Long,
+    val name : String,
+    val thumbnail: String,
+    val comicCount: Int,
+    val description: String?
+)
