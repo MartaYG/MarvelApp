f92a60f7bd9fd4ee02088dc34b797d14d1a09f3c
myebragonzalez <myebra@hiberus.com>
2023-05-31 21:55:03 +0200

    Get character details from the api

--- app/src/main/java/com/myebra/marvelapp/data/datasource/features/character/remote/api/CharacterService.kt
+++ app/src/main/java/com/myebra/marvelapp/data/datasource/features/character/remote/api/CharacterService.kt
@@ -3,6 +3,7 @@ package com.myebra.marvelapp.data.datasource.features.character.remote.api
 import com.myebra.marvelapp.BuildConfig
 import com.myebra.marvelapp.data.datasource.features.character.remote.models.RemoteAllCharacters
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterService {
@@ -14,5 +15,13 @@ interface CharacterService {
         @Query("ts") ts : Int = 1,
         @Query("apikey") apikey : String = BuildConfig.API_KEY,
         @Query("hash") hash : String = BuildConfig.HASH
+    ): RemoteAllCharacters
+
+    @GET("characters/{characterId}")
+    suspend fun getCharacterDetails(
+        @Path("characterId") characterId:Int,
+        @Query("ts") ts : Int = 1,
+        @Query("apikey") apikey : String = BuildConfig.API_KEY,
+        @Query("hash") hash : String = BuildConfig.HASH,
     ): RemoteAllCharacters
 }
