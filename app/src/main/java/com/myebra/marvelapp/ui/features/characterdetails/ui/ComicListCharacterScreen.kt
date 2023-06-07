b37b4f583bb9e00fa1f6bf0f61e0c893b51bfd6b
myebragonzalez <myebra@hiberus.com>
2023-06-07 10:57:47 +0200

    Modification of the character's list view

--- app/src/main/java/com/myebra/marvelapp/ui/features/characterdetails/ui/ComicListCharacterScreen.kt
+++ app/src/main/java/com/myebra/marvelapp/ui/features/characterdetails/ui/ComicListCharacterScreen.kt
@@ -1,30 +1,28 @@
 package com.myebra.marvelapp.ui.features.characterdetails.ui
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarMonth
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -45,7 +43,7 @@ fun ComicListCharacter(
     resultIdCharacter:Long
 ){
 
-    var loadedComic : List<Comic>? = null
+    val loadedComic : List<Comic>?
     val appContext = LocalContext.current
     val comicState by characterDetailsViewModel.comicState.collectAsStateWithLifecycle()
     characterDetailsViewModel.fetchComicsCharacter(resultIdCharacter)
@@ -65,11 +63,10 @@ fun ComicListCharacter(
                     Text(
                         text = stringResource(R.string.comics_character_title),
                         fontWeight = FontWeight.Bold,
-                        overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
                         fontSize = 20.sp,
                         modifier = Modifier
-                            .padding(top = 20.dp)
+                            .padding(top = 20.dp, start = 20.dp)
                     )
                     LazyRow(
                         modifier = Modifier
@@ -86,62 +83,59 @@ fun ComicListCharacter(
         else->{}
     }
 }
-
 @Composable
-fun ComicCharacterItem(item: Comic){
-
-    Card(
+fun ComicCharacterItem(item: Comic) {
+    Box(
         modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .height(280.dp)
             .width(200.dp)
-            .height(300.dp)
-            .padding(16.dp),
-        shape = MaterialTheme.shapes.medium,
+            .clip(shape = RoundedCornerShape(4.dp))
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 6.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+        Box(
+            modifier = Modifier.fillMaxSize()
         ) {
             AsyncImage(
                 model = item.thumbnail,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(150.dp)
-                    .clip(shape = RoundedCornerShape(4.dp))
+                modifier = Modifier.fillMaxSize()
             )
-
-            Column(
+            Box(
                 modifier = Modifier
-                    .fillMaxHeight()
+                    .fillMaxSize()
+                    .background(Color.Black.copy(alpha = 0.5f))
+                    .padding(16.dp),
+                contentAlignment = Alignment.BottomStart
             ) {
-                Text(
-                    text = item.title,
-                    fontWeight = FontWeight.Bold,
-                    overflow = TextOverflow.Ellipsis,
-                    maxLines = 1,
-                    fontSize = 20.sp,
-                    modifier = Modifier
-                        .padding(top = 20.dp, start = 10.dp)
-                )
-                Row(
-                    modifier = Modifier
-                        .padding(top = 10.dp)
-
-                ) {
-                    Icon(
-                        Icons.Filled.CalendarMonth,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(start = 10.dp)
-                    )
+                Column(modifier = Modifier.fillMaxWidth()) {
                     Text(
-                        text = item.onSaleDate,
+                        text = item.title,
+                        fontWeight = FontWeight.Bold,
                         overflow = TextOverflow.Ellipsis,
                         maxLines = 1,
+                        fontSize = 20.sp,
+                        color = Color.White,
                     )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(bottom = 10.dp)
+                    ) {
+                        Icon(
+                            Icons.Filled.CalendarMonth,
+                            contentDescription = null,
+                            tint = Color.White,
+                            modifier = Modifier.padding(end = 4.dp)
+                        )
+                        Text(
+                            text = item.onSaleDate,
+                            overflow = TextOverflow.Ellipsis,
+                            maxLines = 1,
+                            color = Color.White
+                        )
+                    }
                 }
             }
         }
